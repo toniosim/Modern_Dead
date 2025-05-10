ada5baae8d2d745fd32853f716d9bfa5bfed300d
toniosim <tonio.simonetta@gmail.com>
2025-05-10 02:54:50 -0500

    added action log

---
+++ src/components/messages/ActionLog.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="action-log">
+    <q-card>
+      <q-card-section>
+        <div class="text-subtitle1">Game Log</div>
+        <q-separator />
+          <q-item-section>
+            <q-item-label caption>
+              01:32 AM
+            </q-item-label>
+            <q-item-label>
+              Here's one!
+            </q-item-label>
+          </q-item-section>
+      </q-card-section>
+    </q-card>
+  </div>
+
+</template>
+
+<style scoped>
+
+</style>
+
+<script setup lang="ts">
+
+</script>
